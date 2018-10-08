63425800dfc8f763a3fc048322d767561127213e
sivsnkr <shivshankar@Shivs-MacBook-Air-2.local>
2018-10-08 16:24:11 +0530

    Add firebase npm to app

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require("express"),
+firebase = require("firebase"),
 mongoose = require("mongoose"),
 methodOverride = require("method-override"),
+bodyParser = require("body-parser"),
 app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
     var friends = ["Shiv Shankar", "Krishna Kumar Sutar", "Aviral Rai", "Bazif Rasool"];
     res.render("home.ejs", {friends: friends});
+})
+
+app.get("/:hell", function(req,res){
+    res.send("Hello Shiv");
 })
 
 
