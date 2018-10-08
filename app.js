82180cd4601a0bbf874345c3b3d2fcaf040c3a3d
sivsnkr <shivshankar@Shivs-MacBook-Air-2.local>
2018-10-08 17:01:08 +0530

    Add firebase credential

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express"),
 firebase = require("firebase"),
+request = require("request"),
 mongoose = require("mongoose"),
 methodOverride = require("method-override"),
 bodyParser = require("body-parser"),
@@ -7,6 +8,34 @@ app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+//accessing the firebase 
+var config = {
+    apiKey: "AIzaSyCPFJQqBKcXUlQz19bvW4auQQgmT3FQU4s",
+    authDomain: "munshik3-46360.firebaseapp.com",
+    databaseURL: "https://munshik3-46360.firebaseio.com",
+    projectId: "munshik3-46360",
+    storageBucket: "munshik3-46360.appspot.com",
+    messagingSenderId: "684242987795"
+  };
+  firebase.initializeApp(config);
+  //accessing the firebase ends here
+
+  //all the routes start here
+
+//code for getting the json file of firebase
+request("https://www.gstatic.com/firebasejs/5.5.3/firebase.js", function(err,response, body){
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        if(response.statusCode === 200)
+        {
+            console.log(body);
+        }
+    }
+})
 app.get("/", function(req,res){
     var friends = ["Shiv Shankar", "Krishna Kumar Sutar", "Aviral Rai", "Bazif Rasool"];
     res.render("home.ejs", {friends: friends});
