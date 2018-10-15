e1e3f9a8902dd46b61de03c539283d06ec493456
sivsnkr <shivshankar@Shivs-MacBook-Air-2.local>
2018-10-15 20:38:27 +0530

    Add data to mongoDB without Schema

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ bodyParser = require("body-parser"),
 app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+//connecting to mongoDB
+mongoose.connect("mongodb://localhost/InternTestData");
 
 //accessing the firebase 
 var config = {
@@ -21,11 +23,34 @@ var initialization = firebase.initializeApp(config);
 //accessing the firebase ends here
 //accessing the firestore
 const firestore = firebase.firestore();
-var data = firestore.collection("barcode_inventory");
+const settings = {/* your settings... */ timestampsInSnapshots: true};
+firestore.settings(settings);
 
-data.get().then(function(doc){
-    var userData = doc._snapshot.docChanges[0].doc.data;
-    console.log(userData);
+
+//barcode_inventory starts here
+var barcode_inventory_data = firestore.collection("barcode_inventory");
+//creating the schema for mongoose
+var dataSchema = new mongoose.Schema({},{"strict":false});
+var dataTable = mongoose.model("barcodeInventory",dataSchema);
+barcode_inventory_data.get().then(function(doc){
+    var userData; //= doc._snapshot.docChanges[0].doc.data;
+    var collections = doc._snapshot.docChanges;
+    collections.forEach(function(Mdata){
+        userData = Mdata.doc.data;
+        if(userData)
+        {
+            var data = {
+                barcode_number: userData.internalValue.root.value.internalValue,
+                barcode_name: userData.internalValue.root.left.value.internalValue,
+                barcode_price: userData.internalValue.root.right.left.value.internalValue,
+                timestamp: userData.internalValue.root.right.value.internalValue
+            }
+            console.log(data);
+            dataTable.create(data,function(data){
+                console.log(data);
+            })
+        }
+    })
 })
 
 
