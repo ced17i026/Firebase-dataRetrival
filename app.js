511f33ba9b820997115ef7d0781202ccb0f5a6d4
sivsnkr <shivshankar@Shivs-MacBook-Air-2.local>
2018-10-19 22:08:40 +0530

    Add customers data

--- app.js
+++ app.js
@@ -49,19 +49,37 @@ barcode_inventory_data.get().then(function(doc){
 var usersData = firestore.collection("users");
 //creating the schema for mongoose
 var usersdataSchema = new mongoose.Schema({},{"strict":false});
-var usersdataTable = mongoose.model("barcodeInventory",dataSchema);
+var usersdataTable = mongoose.model("usersData",usersdataSchema);
 usersData.get().then(function(doc){
     var collections = doc._snapshot.docChanges;
     collections.forEach(function(Mdata)
     {
         var data = Mdata.doc.data.internalValue.root;//this is data that has to be inserted to mongo
         console.log(data);
-        dataTable.create(data,function(data){
+        usersdataTable.create(data,function(data){
             //data is inserted
         })
         console.log("------------------------------");
     })
 })
+
+//getting the costumers data
+var customersList = firestore.collection("customers");
+//creating the schema for mongoose
+var customersListSchema = new mongoose.Schema({},{"strict":false});
+var customersListTable = mongoose.model("customersList",customersListSchema);
+customersList.get().then(function(doc){
+    var collections = doc._snapshot.docChanges;
+    collections.forEach(function(Mdata)
+    {
+        var data = Mdata.doc.data.internalValue.root;//this is data that has to be inserted to mongo
+        console.log(data);
+        customersListTable.create(data,function(data){
+            //data is inserted
+        })
+        console.log("------------------------------");
+    })
+}) 
 
 
 app.listen(7000, function(req,res){
