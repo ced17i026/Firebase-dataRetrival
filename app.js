56ea3f7869296dd018771355dc44fffba777e12b
sivsnkr <shivshankar@Shivs-MacBook-Air-2.local>
2018-10-19 21:57:55 +0530

    Add users data to mongo

--- app.js
+++ app.js
@@ -27,28 +27,39 @@ const settings = {/* your settings... */ timestampsInSnapshots: true};
 firestore.settings(settings);
 
 
-//barcode_inventory starts here
+//getting the barcode_inventory data
 var barcode_inventory_data = firestore.collection("barcode_inventory");
 //creating the schema for mongoose
 var dataSchema = new mongoose.Schema({},{"strict":false});
 var dataTable = mongoose.model("barcodeInventory",dataSchema);
 barcode_inventory_data.get().then(function(doc){
-    var userData; //= doc._snapshot.docChanges[0].doc.data;
     var collections = doc._snapshot.docChanges;
-    collections.forEach(function(Mdata){
-        userData = Mdata.doc.data;
-        if(userData)
-        {
-            var data = {
-                barcode_number: userData.internalValue.root.value.internalValue,
-                barcode_name: userData.internalValue.root.left.value.internalValue,
-                barcode_price: userData.internalValue.root.right.left.value.internalValue,
-                timestamp: userData.internalValue.root.right.value.internalValue
-            }
-            dataTable.create(data,function(data){
-                //data is inserted
-            })
-        }
+    collections.forEach(function(Mdata)
+    {
+        var data = Mdata.doc.data.internalValue.root;//this is data that has to be inserted to mongo
+        console.log(data);
+        dataTable.create(data,function(data){
+            //data is inserted
+        })
+        console.log("------------------------------");
+    })
+})
+
+//getting the user data
+var usersData = firestore.collection("users");
+//creating the schema for mongoose
+var usersdataSchema = new mongoose.Schema({},{"strict":false});
+var usersdataTable = mongoose.model("barcodeInventory",dataSchema);
+usersData.get().then(function(doc){
+    var collections = doc._snapshot.docChanges;
+    collections.forEach(function(Mdata)
+    {
+        var data = Mdata.doc.data.internalValue.root;//this is data that has to be inserted to mongo
+        console.log(data);
+        dataTable.create(data,function(data){
+            //data is inserted
+        })
+        console.log("------------------------------");
     })
 })
 
